379f4d8b1549dba8504d42c21217a90867750b53
NeyleCM <neylecmorantes@gmail.com>
2024-06-12 23:49:46 +0200

    Actualización DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
  // Aquí tu código
+
+
+ const boton = document.getElementById('agregar');
+ const lista = document.getElementById('lista')
+
+ boton.addEventListener('click', function() {
+    const listaUno = prompt("Tu lista");
+    if (listaUno) {
+        const li = document.createElement('li');
+        li.textContent = listaUno;
+        lista.appendChild(li);
+    }
+});
+
